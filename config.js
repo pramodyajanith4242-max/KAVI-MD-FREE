306892dae72f9eefd3e05b9160f27ac13152bc65
janimd424 <janimd4242@gmail.com>
2025-08-06 19:58:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "eY01gQQC#rue5eA04Q1gKJtbMtikVYoKeFitX9yrzB7yAtOUEXBo", // ඔයාගෙ session id එක
+SESSION_ID: process.env.SESSION_ID || "3YEEQIYA#6jkc3SOUngY4D9cyPtNCefpmNaKXFMb6d3syWyTfI1E", // ඔයාගෙ session id එක
 MONGODB: process.env.MONGODB || "mongodb://mongo:JFbSPuHLyjzRSNJDKiiKPtVqdhGniipI@shuttle.proxy.rlwy.net:56465",  //ඔයාගෙ public mongoDb url එක
 };
  
